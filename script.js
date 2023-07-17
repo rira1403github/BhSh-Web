9df20dc1a468b8b385eebb03c31c99f15ea3d12c
Ritik Raj <126543589+rira1403github@users.noreply.github.com>
2023-07-17 23:21:20 +0530

    My Project
    Practice work.

---
+++ script.js
@@ -0,0 +1,7 @@
+/* ======== typing animation =========*/
+var typed = new Typed(".typing", {
+    strings:["","Artificial Intelligence.","Computer Engineer.","Programmer.","Psychologist."],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
